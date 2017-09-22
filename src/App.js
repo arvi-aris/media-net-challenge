5889229b98a9cf9a4ab529b7a750aae6425c965c
Aravindh Nagarajan <aravindh.nagarajan@hotmail.com>
2017-09-22 16:02:32 +0530

    Initial base files commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <AppBar
+            title="Media.net challenge"
+          />
+      </div>
+    );
+  }
+}
+
+export default App;
